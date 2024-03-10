b2bd2f138160d60811c88cf36b003e20cbbdca0d
Pedro Tavares <pedro.fernandes_tavares@maxiv.lu.se>
2024-03-10 15:50:25 +0100

    new

---
+++ Additions_PFT/pubtools/plotEAperture.m
@@ -0,0 +1,18 @@
+function  EApertures=plotEAperture(RING)
+% Plots the Rectangular aperture fields 
+% 
+nelem = length(RING);
+Spos=findspos(RING,1:nelem);
+EApertures=zeros(nelem,2);
+for i=1:nelem
+    if (isfield(RING{i},'EApertures'))
+        EApertures(i,1:2) = RING{i}.EApertures;
+    else
+        EApertures(i,1:2) = nan(1,2);
+    end
+end
+figure;plot(Spos,EApertures(:,1)*1000,'-ob');hold;xlabel('S[m]');ylabel('Aperture[mm]');
+ylim([0,20]);
+plot(Spos,EApertures(:,2)*1000,'-sr');
+legend('X','Y');grid;
+end
