ede1bb3eb843636e673e87b0ee57e4d2eb4036d7
Marco Apollonio <marco.apollonio@maxiv.lu.se>
2024-09-06 14:55:32 +0200

    silly change for test

--- atmat/atdemos/atmatchExamples/ExampleATMATCH/VaryQuadFam.m
+++ atmat/atdemos/atmatchExamples/ExampleATMATCH/VaryQuadFam.m
@@ -2,7 +2,7 @@ function R=VaryQuadFam(R,K1val,fam)
 %
 % functions returns a new ring with a different value for K1 of fam
 
-indfam=findcells(R,'FamName',fam);
+indfam=findcells(R,'FamName',fam); %
 %*ones(size(indfam))
 R=setcellstruct(R,'PolynomB',indfam,K1val*ones(size(indfam)),1,2); 
 R=setcellstruct(R,'K',indfam,K1val*ones(size(indfam)));
